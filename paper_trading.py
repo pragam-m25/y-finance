ddd470f257aabfbd14762be16f72b0d906be7b49
pragam-m25 <pragam279@gmail.com>
2025-12-18 11:05:59 +0530

    adding decsion making function

--- paper_trading.py
+++ paper_trading.py
@@ -6,3 +6,12 @@ balance=10000
 is_stock_held=False
 # Step 2: Loop aur Live Data (Dil ki Dhadkan)
 # Ab humein wo loop banana hai jo har minute  market se naya data laaye 
+def making_decision():
+    while True:
+        print("checking Market...")
+        data=sbin.history(period="1d",interval="1m")
+        data['SMA5']=data['close'].rolling(window=5).mean()
+        current_price=data["close"].iloc[-1]
+        current_sma=data["SMA5"].iloc[-1]
+        
+        print(f"Price: {current_price} | SMA: {current_sma}")
