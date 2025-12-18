4c96ce990c04f107a09de38cc92dfd6a7159589b
pragam-m25 <pragam279@gmail.com>
2025-12-18 10:58:39 +0530

    adding changes

--- paper_trading.py
+++ paper_trading.py
@@ -2,3 +2,7 @@ import yfinance as yf
 import time
 # ticker banaya kyuki ye us stock ka id card hain iski help se wo data lekar ayega wo pura
 sbin=yf.Ticker("SBIN.NS")
+balance=10000
+is_stock_held=False
+# Step 2: Loop aur Live Data (Dil ki Dhadkan)
+# Ab humein wo loop banana hai jo har minute  market se naya data laaye 
